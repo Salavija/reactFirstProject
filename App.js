dbbaa3d452cab703b2d2f793a0f344a46cdb1f40
Saheya <theaura8@gmail.com>
2018-11-20 12:00:50 +0200

    Corection

--- App.js
+++ App.js
@@ -3,3 +3,10 @@ console.log('Pasileido')
 for (var i=0; i<10; i++){
     console.log('Pasileido' + i)
 }
+
+const component = <h1>Hello, world</h1>;
+
+ReactDOM.render(
+    component,
+    document.getElementById('root')
+);
