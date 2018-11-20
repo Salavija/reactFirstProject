d9929499963d302187f8f7cacee24f5c2399c174
Saheya <theaura8@gmail.com>
2018-11-20 12:25:44 +0200

    React links written correctly

--- App.js
+++ App.js
@@ -10,3 +10,14 @@ ReactDOM.render(
     component,
     document.getElementById('root')
 );
+
+class HelloComponent extends React.Component{ 
+    render() {
+        return <div>Hello {this.props.name} </div>;
+    }
+}
+
+ReactDOM.render(
+    <HelloComponent name="Jane"/>,
+    document.getElementById('root')
+    );
